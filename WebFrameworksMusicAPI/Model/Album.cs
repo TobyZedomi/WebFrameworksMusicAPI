71cf644763aa72809357453355f62c9d00c3223e
TobyZedomi <tobyzedo4@gmail.com>
2025-02-03 21:32:26 +0000

    Album class

---
+++ WebFrameworksMusicAPI/Model/Album.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace WebFrameworksMusicAPI.Model
+{
+    public class Album
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string AlbumName { get; set; }
+
+        [Range(0,50)]
+        public int NumberOfSongs { get; set; }
+
+        [Range(0,50)]
+        public int NumberOfFeatures { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
