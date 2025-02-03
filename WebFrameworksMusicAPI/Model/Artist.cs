a869d2d58b63bee9f37baedbe8a38bb0c8f381a8
TobyZedomi <tobyzedo4@gmail.com>
2025-02-03 21:17:37 +0000

    Artist class

---
+++ WebFrameworksMusicAPI/Model/Artist.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebFrameworksMusicAPI.Model
+{
+    public class Artist
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string ArtistName { get; set; }
+
+        [MaxLength(50)]
+        public string Genre { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+
+    }
+}
