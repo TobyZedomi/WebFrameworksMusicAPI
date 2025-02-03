ca3175309c07cc475882272d4d4fc21818e91cf2
TobyZedomi <tobyzedo4@gmail.com>
2025-02-03 21:44:57 +0000

    DbContext class

---
+++ WebFrameworksMusicAPI/Data/DbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebFrameworksMusicAPI.Model;
+
+namespace WebFrameworksMusicAPI.Data
+{
+    public class DbContext : IdentityDbContext<IdentityUser>
+    {
+
+
+        public DbSet<Album> Album { get; set; }
+
+        public DbSet<Artist> Artist { get; set; }
+
+        public DbSet<Song> Song { get; set; }
+
+
+        public DbContext(DbContextOptions options) : base(options)
+        {
+
+            Database.EnsureCreated();
+
+        }
+
+
+    }
+}
