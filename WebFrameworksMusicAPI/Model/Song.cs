4de44dcb59ef5b7109391daa497b1f8368a81a60
TobyZedomi <tobyzedo4@gmail.com>
2025-02-03 21:37:56 +0000

    Song class

---
+++ WebFrameworksMusicAPI/Model/Song.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebFrameworksMusicAPI.Model
+{
+    public class Song
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(200)]
+        public string SongName { get; set; }
+        public Artist? Artist { get; set; }
+        public Album? Album { get; set; }
+
+    }
+}
